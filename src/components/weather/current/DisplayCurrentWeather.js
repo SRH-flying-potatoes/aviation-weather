f199610242916d8f8bfc94fa602e3c67e7a7ed68
Shubham Bajaj <shubham2bajaj@gmail.com>
2023-05-12 10:07:53 +0200

    added text data

--- src/components/weather/current/DisplayCurrentWeather.js
+++ src/components/weather/current/DisplayCurrentWeather.js
@@ -14,7 +14,22 @@ export default class DisplayCurrentWeather extends Component {
     return (
       <div className="air-weather">
         <div className="air-weather-info">
+          <div className="air-location-info">
+            <p>{`name`}</p>
+            <p>{`region`}</p>
+            <p>{`country`}</p>
+          </div>
           <h3>{this.state.weather.condition.text}</h3>
+          <p>{`last_updated`}</p>
+          <p>{`temp_c`}</p>
+          <p>{`wind_kph`}</p>
+          <p>{`wind_degree`}</p>
+          <p>{`wind_dir`}</p>
+          <p>{`pressure_mb`}</p>
+          <p>{`precip_mm`}</p>
+          <p>{`vis_km`}</p>
+          <p>{`humidity`}</p>
+          <p>{`gust_kph`}</p>
         </div>
         <div className="air-map-container">
           <MyMap
